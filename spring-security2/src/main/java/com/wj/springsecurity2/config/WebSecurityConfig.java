3b943db617f183fc9a96edf6b34dc32fc611990b
汪进 <1018863434@qq.com>
2017-08-16 09:58:10 +0800

    忽略静态资源的拦截;

--- spring-security2/src/main/java/com/wj/springsecurity2/config/WebSecurityConfig.java
+++ spring-security2/src/main/java/com/wj/springsecurity2/config/WebSecurityConfig.java
@@ -35,13 +35,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().authenticated() //任何请求,登录后可以访问
+                .anyRequest().authenticated() //任何请求,登录后可以访问,对任何尚未匹配的URL进行身份验证
+                //忽略一些资源的访问权限:如静态资源
+                .antMatchers(new String[]{"/js/**", "/css/**", "/img/**", "/images/**", "/fonts/**", "/**/favicon.ico"}).permitAll()
                 .and()
-                .formLogin()
-                .loginPage("/login")
-                .failureUrl("/login?error")
-                .permitAll() //登录页面用户任意访问
+                // 登录地址，成功跳转地址，登录失败地址
+                // .formLogin().loginPage("/login").defaultSuccessUrl("/", true).failureUrl("/login?error")
+                .formLogin().loginPage("/login").failureUrl("/login?error")
+                .permitAll() //"登录页面"未登录用户也可以访问
                 .and()
-                .logout().permitAll(); //注销行为任意访问
+                .logout().permitAll();  //注销行为任意访问
+        // http
+        //         .addFilterAfter(getMyFilterSecurityInterceptor(), FilterSecurityInterceptor.class);
     }
+
+    // @Override
+    // public void configure(WebSecurity web) throws Exception {
+    //忽略一些资源的访问权限:如静态资源
+    // web.ignoring().antMatchers("/js/**", "/css/**", "/img/**", "/images/**", "/fonts/**", "/**/favicon.ico");
+    // }
 }
