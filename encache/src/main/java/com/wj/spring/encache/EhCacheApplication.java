d23cddf41cc70819ac10498341a566f14b7469da
汪进 <1018863434@qq.com>
2017-08-17 10:23:07 +0800

    修改错误逻辑;

--- encache/src/main/java/com/wj/spring/encache/EhCacheApplication.java
+++ encache/src/main/java/com/wj/spring/encache/EhCacheApplication.java
@@ -1,6 +1,5 @@
 package com.wj.spring.encache;
 
-import com.wj.spring.encache.controller.DemoInfoController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EhCacheApplication {
     public static void main(String[] args) {
         SpringApplication.run(EhCacheApplication.class, args);
-        DemoInfoController demoInfoController = new DemoInfoController();
-        demoInfoController.test();
     }
 }
